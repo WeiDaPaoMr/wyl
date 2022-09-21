add8c3dc5f5d2c50caefd56f0b2cee3c5c72501a
魏大炮 <3378923215@qq.com>
2022-09-21 21:34:08 +0800

    docs(custom): 配置跨域

--- vue.config.js
+++ vue.config.js
@@ -3,10 +3,20 @@ const {
 } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: '/',
   devServer: {
     // port: 0000,
     host: "localhost",
     open: true,
-    https: false
+    https: false,
+    proxy: {
+      [process.env.VUE_APP_BASE_API]: {
+        target: process.env.VUE_APP_SERVICE_URL,
+        changeOrigin: true,
+        pathRewrite: {
+          ["^" + process.env.VUE_APP_BASE_API]: ''
+        }
+      }
+    }
   }
 })
