07914fac2de2f69c8237a30895e625d5ca10a87b
魏大炮 <3378923215@qq.com>
2022-09-22 08:21:04 +0800

    docs(custom): 跨域地址

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.use(ElementUI);
-
+console.log(process.env);
 import axios from "axios"
 Vue.prototype.$axios = axios
 
--- vue.config.js
+++ vue.config.js
@@ -4,11 +4,24 @@ const {
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: '/',
+  // devServer: {
+  //   host: "localhost",
+  //   open: true,
+  //   proxy: {
+  //     '/dev-api': {
+  //       target: "http://localhost:3000",
+  //       changeOrigin: true,
+  //       pathRewrite: {
+  //         '/dev-api': ""
+  //       }
+  //     }
+  //   }
+  // }
+
   devServer: {
-    // port: 0000,
+    port: 8888,
     host: "localhost",
     open: true,
-    https: false,
     proxy: {
       [process.env.VUE_APP_BASE_API]: {
         target: process.env.VUE_APP_SERVICE_URL,
