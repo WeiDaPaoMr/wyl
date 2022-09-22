0492f3861599c3082f68db1cedcee33bd4c4699b
魏大炮 <3378923215@qq.com>
2022-09-22 23:39:15 +0800

    feat(custom): 登陆页面静态布局

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Login from '../views/login/index.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
+const routes = [{
+    path: '/home',
     name: 'home',
-    component: HomeView
+    component: () => import('../views/HomeView.vue')
   },
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import('../views/AboutView.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
   }
 ]
 
---
+++ src/views/login/index.vue
@@ -0,0 +1,80 @@
+
+<template>
+  <div class="bigbox">
+    <div class="login">
+      <h2 class="title">积云会员管理系统</h2>
+      <el-form :rules="rules" ref="form" :model="loginForm" label-width="60px">
+        <el-form-item label="账号" prop="username">
+          <el-input v-model.trim="loginForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model.trim="loginForm.password"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="handleLoginSubmit">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "",
+  components: {},
+  data() {
+    return {
+      loginForm: {
+        username: "",
+        password: "",
+      },
+      rules: {
+        username: [
+          { required: true, message: "账号不能为空", trigger: "blur" },
+          { min: 3, max: 12, message: "请输入3～12个字符", trigger: "blur" },
+        ],
+        password: [
+          { required: true, message: "密码不能为空", trigger: "blur" },
+        ],
+      },
+    };
+  },
+  created() {},
+  mounted() {},
+  methods: {
+    handleLoginSubmit() {
+      this.$refs["form"].validate((valid) => {
+        if (!valid) return;
+        // 校验通过之后执行登录方法
+        this.handleLogin();
+      });
+    },
+  },
+};
+</script>
+<style scoped lang='scss'>
+.bigbox {
+  width: 100%;
+  height: 100%;
+  background: url("http://vue.mengxuegu.com/img/login.b665435f.jpg");
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  overflow: hidden;
+}
+.login {
+  width: 350px;
+  background-color: rgba(255, 255, 255, 0.8);
+  margin: 160px auto;
+  border-radius: 20px;
+  padding: 28px;
+}
+.title {
+  font-size: 24px;
+  font-weight: 700;
+  text-align: center;
+  padding-top: 20px;
+}
+.el-form {
+  margin-top: 20px;
+}
+</style>
