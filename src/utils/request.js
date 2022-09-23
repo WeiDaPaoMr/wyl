8c486c4e56dde73f203545aacbe8efadf748b185
魏大炮 <3378923215@qq.com>
2022-09-23 10:45:26 +0800

    feat(custom): 调用登录用户接口

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,6 +4,12 @@ const serverce = axios.create({
     baseURL: process.env.VUE_APP_SERVICE_URL,
     timeout: 5000
 })
+
+const errormessage = {
+    1000: "用户名或者密码发生错误",
+    2000: "XXX发生错误",
+    3000: ''
+}
 //请求拦截s
 serverce.interceptors.request.use(function (config) {
     return config;
@@ -12,18 +18,20 @@ serverce.interceptors.request.use(function (config) {
 });
 //响应拦截
 
-const _showError = () => {
-    this.$message.error('出现错误');
+const _showError = (errorcode, message) => {
+    let titlt
+    titlt = errormessage[errorcode] || message || "发生未知错误"
+    this.$message.error(title);
 }
 
 serverce.interceptors.response.use(function (response) {
-    if (response.status === 200) {
+    if (response.status < 400) {
         return response.data.data
     }
     if (response.status === 401) {
         return
     }
-    _showError()
+    _showError(response.data.code, response.data.message)
 }, function (error) {
     return Promise.reject(error);
 });
